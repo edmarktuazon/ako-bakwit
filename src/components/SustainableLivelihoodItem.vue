3c637fe98c20a294de0ce984a4c24a3458a48e15
edmarktuazon <edmarktuazon03@gmail.com>
2026-02-25 23:58:03 +0800

    chore: remove class in parent divwith slot child

--- src/components/SustainableLivelihoodItem.vue
+++ src/components/SustainableLivelihoodItem.vue
@@ -2,29 +2,27 @@
 
 <template>
   <section class="layout-wrapper bg-brand-white-secondary" id="sustainable-livelihood">
-    <div class="section-grid-cols-2">
-      <div class="row-end-3 lg:row-end-auto">
-        <div class="mb-16">
-          <h2 class="section-heading text-brand-black leading-snug">
-            <slot name="section-heading"></slot>
-          </h2>
-          <div class="heading-underline"></div>
-        </div>
-
-        <div class="flex flex-col w-4/5">
-          <p class="text-brand-black font-brand leading-relaxed text-base">
-            <slot name="sub-description-01"></slot>
-          </p>
-          <hr class="border-white/20 w-full mx-auto my-8" />
-
-          <p class="text-brand-black font-brand leading-relaxed text-base">
-            <slot name="sub-description-02"></slot>
-          </p>
-        </div>
+    <div class="grid lg:grid-cols-2 gap-8 items-start">
+      <!-- Title -->
+      <div class="col-span-full lg:col-span-2 mb-4">
+        <h2 class="section-heading text-brand-black leading-snug">
+          <slot name="section-heading"></slot>
+        </h2>
+        <div class="heading-underline"></div>
       </div>
 
-      <!-- Sustainable Livelihood Model Image -->
-      <div data-aos="zoom-out" data-aos-easing="ease-in" data-aos-delay="300">
+      <!-- Texts -->
+      <div class="flex flex-col gap-4">
+        <p class="text-brand-black font-brand leading-relaxed text-base">
+          <slot name="sub-description-01"></slot>
+        </p>
+        <hr class="border-gray-300 w-full mx-auto my-4" />
+        <p class="text-brand-black font-brand leading-relaxed text-base">
+          <slot name="sub-description-02"></slot>
+        </p>
+      </div>
+      <!-- Image -->
+      <div class="" data-aos="zoom-out" data-aos-easing="ease-in" data-aos-delay="300">
         <slot name="sustainable-livelihood-img-model"></slot>
       </div>
     </div>
